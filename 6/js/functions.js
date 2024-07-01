92dca1d472af6053fceed8d64f293c9b0cb86aa5
Keks <keks@htmlacademy.ru>
2024-07-01 21:28:40 +0300

    :heavy_check_mark: Сборка #6

---
+++ 6/js/functions.js
@@ -0,0 +1,69 @@
+const MINUTES_IN_HOUR = 60;
+
+const isValidStringLength = (str, maxLength) => str.length <= maxLength;
+
+// Строка короче 20 символов
+isValidStringLength('проверяемая строка', 20); // true
+// Длина строки ровно 18 символов
+isValidStringLength('проверяемая строка', 18); // true
+// Строка длиннее 10 символов
+isValidStringLength('проверяемая строка', 10); // false
+
+const isPalindrome = (str) => {
+  const normalizeStr = str.replaceAll(' ', '').toLowerCase();
+  const reversedNormalizeStr = normalizeStr.split('').reverse().join('');
+
+  return normalizeStr === reversedNormalizeStr;
+};
+
+// Строка является палиндромом
+isPalindrome('топот'); // true
+// Несмотря на разный регистр, тоже палиндром
+isPalindrome('ДовОд'); // true
+// Это не палиндром
+isPalindrome('Кекс'); // false
+// Это точно палиндром
+isPalindrome('Лёша на полке клопа нашёл '); // true
+
+const extractDigitsToNumber = (value) => {
+  const digits = String(value).match(/\d/g);
+  return digits ? parseInt(digits.join(''), 10) : NaN;
+};
+
+extractDigitsToNumber('2023 год'); // 2023
+extractDigitsToNumber('ECMAScript 2022'); // 2022
+extractDigitsToNumber('1 кефир, 0.5 батона'); // 105
+extractDigitsToNumber('агент 007'); // 7
+extractDigitsToNumber('а я томат'); // NaN
+extractDigitsToNumber(2023); // 2023
+extractDigitsToNumber(-1); // 1
+extractDigitsToNumber(1.5); // 15
+
+const convertTimeToMinutes = (time) => {
+  const [hours, minutes] = time.split(':').map(Number);
+
+  return hours * MINUTES_IN_HOUR + minutes;
+};
+
+const isMeetingDuringWorkHours = (
+  startWorkDay,
+  endWorkDay,
+  startTime,
+  meetDuration
+) => {
+  const startWorkMinutes = convertTimeToMinutes(startWorkDay);
+  const endWorkMinutes = convertTimeToMinutes(endWorkDay);
+  const startMeetingMinutes = convertTimeToMinutes(startTime);
+  const endMeetingMinutes = startMeetingMinutes + meetDuration;
+
+  return (
+    startMeetingMinutes >= startWorkMinutes &&
+    endMeetingMinutes <= endWorkMinutes
+  );
+};
+
+isMeetingDuringWorkHours('08:00', '17:30', '14:00', 90); // true
+isMeetingDuringWorkHours('8:0', '10:0', '8:0', 120); // true
+isMeetingDuringWorkHours('08:00', '14:30', '14:00', 90); // false
+isMeetingDuringWorkHours('14:00', '17:30', '08:0', 90); // false
+isMeetingDuringWorkHours('8:00', '17:30', '08:00', 900); // false
---
+++ 6/js/main.js
@@ -0,0 +1,165 @@
+const PHOTOS = 25;
+const MIN_COMMENT_ID = 1;
+const MIN_AVATAR_ID = 1;
+const MAX_AVATAR_ID = 6;
+const MIN_COMMENTS = 0;
+const MAX_COMMENTS = 30;
+const MIN_LIKES = 15;
+const MAX_LIKES = 200;
+const MIN_MESSAGE_PARAGRAPHS = 1;
+const MAX_MESSAGE_PARAGRAPHS = 2;
+const NAMES = [
+  'Александр',
+  'Михаил',
+  'Дмитрий',
+  'Максим',
+  'Сергей',
+  'Андрей',
+  'Алексей',
+  'Артем',
+  'Илья',
+  'Кирилл',
+  'Никита',
+  'Матвей',
+  'Роман',
+  'Егор',
+  'Арсений',
+  'Иван',
+  'Денис',
+  'Евгений',
+  'Даниил',
+  'Тимофей',
+  'Владислав',
+  'Игорь',
+  'Владимир',
+  'Павел',
+  'Руслан',
+  'Марк',
+  'Константин',
+  'Тимур',
+  'Олег',
+  'Ярослав',
+  'Виктор',
+  'Лев',
+  'Анатолий',
+  'Степан',
+  'Николай',
+  'Глеб',
+  'Федор',
+  'Юрий',
+  'Борис',
+  'Станислав',
+  'Василий',
+  'Валерий',
+  'Антон',
+  'Вячеслав',
+  'Григорий',
+  'Леонид',
+  'Семен',
+  'Петр',
+  'Марат',
+  'Вадим',
+  'Мария',
+  'Анна',
+  'Алина',
+  'Ирина',
+  'Екатерина',
+  'Наталья',
+  'Вероника',
+  'Ольга',
+  'Татьяна',
+  'Юлия',
+];
+const COMMENTS = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
+];
+
+const getRandomInteger = (a, b) => {
+  const lower = Math.ceil(Math.min(a, b));
+  const upper = Math.floor(Math.max(a, b));
+  const result = Math.random() * (upper - lower + 1) + lower;
+
+  return Math.floor(result);
+};
+
+const createRandomIdFromRangeGenerator = (min, max) => {
+  const previousValues = [];
+
+  return function () {
+    let currentValue = getRandomInteger(min, max);
+
+    if (previousValues.length >= max - min + 1) {
+      // eslint-disable-next-line no-console
+      console.error(`Перебраны все числа из диапазона от ${min} до ${max}`);
+      return null;
+    }
+
+    while (previousValues.includes(currentValue)) {
+      currentValue = getRandomInteger(min, max);
+    }
+
+    previousValues.push(currentValue);
+
+    return currentValue;
+  };
+};
+
+const getRandomArrayElement = (elements) =>
+  elements[getRandomInteger(0, elements.length - 1)];
+
+const generateCommentId = createRandomIdFromRangeGenerator(
+  MIN_COMMENT_ID,
+  PHOTOS * MAX_COMMENTS
+);
+
+const createComment = () => {
+  const id = generateCommentId();
+  const avatar = `img/avatar-${getRandomInteger(
+    MIN_AVATAR_ID,
+    MAX_AVATAR_ID
+  )}.svg`;
+  const messagesCount = getRandomInteger(
+    MIN_MESSAGE_PARAGRAPHS,
+    MAX_MESSAGE_PARAGRAPHS
+  );
+  const messages = Array.from({ length: messagesCount }, () =>
+    getRandomArrayElement(COMMENTS)
+  );
+
+  const message = messages.join(' ');
+  const name = getRandomArrayElement(NAMES);
+
+  return {
+    id,
+    avatar,
+    message,
+    name,
+  };
+};
+
+const createPhoto = (_element, index) => {
+  const id = index + 1;
+  const commentsCount = getRandomInteger(MIN_COMMENTS, MAX_COMMENTS);
+  const comments = Array.from({ length: commentsCount }, createComment);
+  const likes = getRandomInteger(MIN_LIKES, MAX_LIKES);
+  const description = 'Описание фотографии';
+  const url = `photos/${id}.jpg`;
+
+  return {
+    id,
+    url,
+    description,
+    likes,
+    comments,
+  };
+};
+
+const photos = Array.from({ length: PHOTOS }, createPhoto);
+
+// eslint-disable-next-line no-console
+console.log(photos);
